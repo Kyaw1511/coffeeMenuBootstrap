4591059bf83dd81af3ad052ca7f4ca9339414c8f
Kyaw1511 <kyawminthein151193@gmail.com>
2024-05-24 21:50:07 +0630

    change main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+let num1 = 0;
+let num2 = 0;
+let num3 = 0;
+
+    // function myIncreaseFun()  {
+    //     document.getElementById("num1").textContent = ++num1;
+    //     document.getElementById("num2").textContent = ++num2;
+    //     document.getElementById("num3").textContent = ++num3;
+    //     console.log(num1)
+        
+    // }
+    // function myDecreaseFun() {
+    //     document.getElementById("num1").textContent = --num1;
+    //     document.getElementById("num2").textContent = --num2;
+    //     document.getElementById("num3").textContent = --num3;
+    //     console.log(num1)
+    // }
+
+    function myIncreaseFun(num1)  {
+                document.getElementById("num1").textContent = ++num1;
+                console.log(num1)
+            
+    }
+    function myIncreaseFun(num2)  {
+        document.getElementById("num2").textContent = ++num2;
+        console.log(num2)
+        
+    }
+    function myIncreaseFun(num3)  {
+        document.getElementById("num3").textContent = ++num3;
+        console.log(num3)
+        
+    }
+    
