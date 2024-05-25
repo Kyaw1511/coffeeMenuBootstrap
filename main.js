9d293f96855c1e4dff4c051e2939adb9656307a5
Kyaw1511 <kyawminthein151193@gmail.com>
2024-05-25 11:40:46 +0630

    change package items price

--- main.js
+++ main.js
@@ -1,34 +1,44 @@
 let orangePackage = 0;
 let bluePackage = 0;
 let greenPackage = 0;
+// let greenPackages = 0;
+
 
     function incOrangePack ()  {
         document.getElementById("orangePackage").textContent = ++orangePackage;
+        document.getElementById("orangePackages").innerHTML = 8000 * orangePackage;
         console.log(orangePackage)
             
     }
     function decOrangePack ()  {
         document.getElementById("orangePackage").textContent = --orangePackage;
+        document.getElementById("orangePackages").innerHTML = 8000 * orangePackage;
         console.log(orangePackage)
             
     }
     function incBluePack ()  {
         document.getElementById("bluePackage").textContent = ++bluePackage;
+        document.getElementById("bluePackages").innerHTML = 12000 * bluePackage;
         console.log(bluePackage)
             
     }
     function decBluePack ()  {
         document.getElementById("bluePackage").textContent = --bluePackage;
+        document.getElementById("bluePackages").innerHTML = 12000 * bluePackage;
         console.log(bluePackage)
             
     }
-    function incGreenPack ()  {
+    incGreenPack = function ()  {
         document.getElementById("greenPackage").textContent = ++greenPackage;
-        console.log(greenPackage)
+        document.getElementById("greenPackages").innerHTML = 15000 * greenPackage;
+        console.log(greenPackage);
+    
             
     }
-    function decGreenPack ()  {
+    decGreenPack = function ()  {
         document.getElementById("greenPackage").textContent = --greenPackage;
-        console.log(greenPackage)
+        document.getElementById("greenPackages").innerHTML = 15000 * greenPackage;
+        console.log(greenPackage);
             
     }
+    
